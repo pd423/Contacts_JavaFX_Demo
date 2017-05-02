266173d46c082f24b4d1a4665f4041c1e0188a5f
Phillip Jhang <phillipx303@gmail.com>
2017-05-02 21:46:49 +0800

    JavaFX 8 教程 - 第一部分：Scene Builder

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Contacts_JavaFX_Demo
+
+This is a practice project for JavaFX and come from [JavaFX 8 教程 （中文）](http://code.makery.ch/library/javafx-8-tutorial/zh-cn/).
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,18 +4,61 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
+    private Stage mPrimaryStage;
+    private BorderPane mRootLayout;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("person_overview.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 300));
-        primaryStage.show();
+        mPrimaryStage = primaryStage;
+        mPrimaryStage.setTitle("Contacts");
+
+        initRootLayout();
+        showPersonOverview();
     }
 
+    private void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+            mRootLayout = loader.load();
+
+            Scene scene = new Scene(mRootLayout);
+            mPrimaryStage.setScene(scene);
+            mPrimaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showPersonOverview() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = loader.load();
+
+            if (mRootLayout != null) {
+                mRootLayout.setCenter(personOverview);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Return the main stage.
+     * @return the main stage
+     */
+    public Stage getPrimaryStage() {
+        return mPrimaryStage;
+    }
 
     public static void main(String[] args) {
         launch(args);
